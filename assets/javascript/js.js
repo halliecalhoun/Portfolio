d2f62c59d6778c99f3abd6582f59d2a2178e5fdc
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-06-18 02:57:09 -0700

    contact form styled with accent color. nav bar and footer links linking to page anchors

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -8,3 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+  $("#contact_form_submit").click(function(e){
+    e.preventDefault();
+    var name = $("#contact_name").val();
+    var email = $("#contact_email").val();
+    var msg = $("#contact_message").val();
+
+    $.ajax({
+      type: "POST",
+      url: "contact.php",
+      data: {
+        'name':name,
+        'email':email,
+        'message':msg,
+      },
+      success: function(data){
+        $("#contact_name").val("");
+        $("#contact_email").val("");
+        $("#contact_message").val("");
+        $("#contact-success").openModal();
+      },
+    });
+});
