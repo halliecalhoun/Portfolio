7a3f5d9fb84251507c693177f9ce09309134420b
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-06-24 10:06:47 -0700

    portfolio in progress with full screen slider added and projects slider added

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,33 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
 
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.parallax').parallax();
-  });
-
-  $("#contact_form_submit").click(function(e){
-    e.preventDefault();
-    var name = $("#contact_name").val();
-    var email = $("#contact_email").val();
-    var msg = $("#contact_message").val();
-
-    $.ajax({
-      type: "POST",
-      url: "contact.php",
-      data: {
-        'name':name,
-        'email':email,
-        'message':msg,
-      },
-      success: function(data){
-        $("#contact_name").val("");
-        $("#contact_email").val("");
-        $("#contact_message").val("");
-        $("#contact-success").openModal();
-      },
-    });
+$(document).ready(function () {
+  $('.parallax').parallax();
 });
+
+$("#contact_form_submit").click(function (e) {
+  e.preventDefault();
+  var name = $("#contact_name").val();
+  var email = $("#contact_email").val();
+  var msg = $("#contact_message").val();
+
+  $.ajax({
+    type: "POST",
+    url: "contact.php",
+    data: {
+      'name': name,
+      'email': email,
+      'message': msg,
+    },
+    success: function (data) {
+      $("#contact_name").val("");
+      $("#contact_email").val("");
+      $("#contact_message").val("");
+      $("#contact-success").openModal();
+    },
+  });
+});
+
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
+
+
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+
